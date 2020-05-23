27ed74d7d7822b59f851c70cc22ac5670b5ba197
Paul Hanyzewski <hanyzewski@gmail.com>
2020-05-23 15:41:31 -0600

    remove uniqueness validation on email

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -1,7 +1,6 @@
 class EmailAddress < ApplicationRecord
   belongs_to :identity
 
-  validates_uniqueness_of :email, case_sensitive: false
   validates :email, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  subject { FactoryBot.build(:email_address) }
+  subject { FactoryBot.create(:email_address) }
 
   it { should belong_to(:identity) }
 
   it { is_expected.to validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email).case_insensitive }
 end
