9f5c98a2b72eda0c1fdba0abe86887486f63c9e1
Paul Hanyzewski <hanyzewski@gmail.com>
2020-05-24 10:33:31 -0600

    Add cmd link which demonstrates searchable fields.  Fix self link to include base_url.

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,22 +1,24 @@
 class AccountSerializer < BaseSerializer
   include FastJsonapi::ObjectSerializer
-  link(:self) { |object| url_helpers.account_path(object) }
+  link(:self) { |object| base_url + url_helpers.account_path(object) }
 
-  has_many :identities, links: {
+  has_many :identities, lazy_load_data: true, links: {
     rel: -> (object){
       base_url + url_helpers.account_identities_path(object.id)
     }
   }
 
-  has_many :vehicles, links: {
+  has_many :vehicles, lazy_load_data: true, links: {
     rel: -> (object){
       base_url + url_helpers.account_vehicles_path(object.id)
     }
   }
 
-  has_many :bookings, links: {
+  has_many :bookings, lazy_load_data: true, links: {
     rel: -> (object){
       base_url + url_helpers.account_bookings_path(object.id)
     }
   }
+
+  link(:cmd){ |object| "#{base_url} #{url_helpers.accounts_search_path}?email_address=,phone_number=,vin=,first_name=,last_name=" }
 end
