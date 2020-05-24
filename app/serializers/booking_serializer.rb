3908521399c5264dcf371283cdc3a516d73a3bb5
Paul Hanyzewski <hanyzewski@gmail.com>
2020-05-24 14:56:50 -0600

    Add serialization for bookings

---
+++ app/serializers/booking_serializer.rb
@@ -0,0 +1,19 @@
+class BookingSerializer < BaseSerializer
+  include ActionView::Helpers::DateHelper
+  include FastJsonapi::ObjectSerializer
+
+  attributes :start_time, :duration, :details
+
+  link(:self) { |object| base_url + url_helpers.booking_path(object) }
+
+  belongs_to :account, links: {
+    rel: -> (object){
+      base_url + url_helpers.account_path(object.account_id)
+    }
+  }
+  belongs_to :vehicle, links: {
+    rel: -> (object){
+      base_url + url_helpers.vehicle_path(object.vehicle_id)
+    }
+  }
+end
