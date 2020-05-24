1872b4a770a99e868d4fbac97f1897339b5c2ba2
Paul Hanyzewski <hanyzewski@gmail.com>
2020-05-24 17:07:37 -0600

    add readme and api documentation

--- README.md
+++ README.md
@@ -1,24 +1,46 @@
-# README
+# Bookings API
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Bookings api is a first class client api not intended as a public interface.
+This api allows a client to create an account and create or associate it with customer identities, vehicles and bookings.
 
-Things you may want to cover:
+This api allows its client to capture all of the above information in a single step, using RESTful resources compliant with open json standards.
 
-* Ruby version
+## Local Setup
 
-* System dependencies
+* Ruby version 2.7.1
 
-* Configuration
+* PostgresDB
 
-* Database creation
+```sh
+bundle install
+``
 
-* Database initialization
+```sh
+bundle exec rails db:create db:migrate
+```
 
-* How to run the test suite
+## Testing
 
-* Services (job queues, cache servers, search engines, etc.)
+```sh
+bundle exec rspec spec/
+```
 
-* Deployment instructions
+## API documentation
 
-* ...
+See doc/api.paw.sh for working examples of interacting with the api.
+```sh
+  /accounts
+    /accounts/search
+    /account/:account_id/vehicles
+    /account/:account_id/vehicles/:id
+    /account/:account_id/bookings
+    /account/:account_id/bookings/:id
+    /account/:account_id/identities
+    /account/:account_id/identities/:id
+  /vehicles
+    /vehicles/:id
+  /bookings
+    /bookings/:id
+  /identities
+    /identities/:id
+```
---
+++ doc/booking_api.paw.sh
@@ -0,0 +1,132 @@
+## Accounts List
+curl "http://localhost:3000/accounts"
+
+## Account Create
+curl -X "POST" "http://localhost:3000/accounts"
+
+## Accounts Search
+curl "http://localhost:3000/accounts/search?email_address=paul%40example.com&first_name=paul&last_name=h&phone_number&vin"
+
+## Account Destroy
+curl -X "DELETE" "http://localhost:3000/accounts/5325ecff-1506-4a40-8138-cd5f838eed49"
+
+## Bookings List
+curl "http://localhost:3000/bookings"
+
+## Bookings Create
+curl -X "POST" "http://localhost:3000/bookings" \
+     -H 'Content-Type: application/json; charset=utf-8' \
+     -d $'{
+  "work_order": "'"'"'{\\"job_id\\":\\"1\\",\\"duration\\":\\"2\\",\\"work\\":[{\\"diag\\":\\"1\\",\\"oil change\\":\\"0.5\\",\\"rotation\\":\\"0.5\\"}]}'"'"'",
+  "vehicle_id": "829362d8-5ac7-4d4f-b832-1086b3576139",
+  "start_time": "\\"2020-05-28 12:00:00 UTC\\"",
+  "account_id": "1342ae0d-2aeb-4537-a9ab-fbf43235c6d1",
+  "duration": "9000"
+}'
+
+## Bookings Update
+curl -X "PATCH" "http://localhost:3000/bookings/e4cde76c-2666-4765-8867-45bc62097eef" \
+     -H 'Content-Type: application/json; charset=utf-8' \
+     -d $'{
+  "start_time": "2020-05-25T18:00:00.000Z",
+  "work_order": "'"'"'{\\"job_id\\":\\"1\\",\\"duration\\":\\"2\\",\\"work\\":[{\\"diag\\":\\"1\\",\\"oil change\\":\\"0.5\\",\\"rotation\\":\\"0.5\\"}]}'"'"'"
+}'
+
+## Bookings Destroy
+curl -X "DELETE" "http://localhost:3000/bookings/f418137c-e827-4b78-888c-30cb4e7696b4"
+
+## Identity Create with account_id
+curl -X "POST" "http://localhost:3000/identities" \
+     -H 'Content-Type: application/json' \
+     -d $'{
+  "account_id": "665b135f-e273-49ce-abe9-0b142c5622ff",
+  "first_name": "existing",
+  "last_name": "account"
+}'
+
+## Identitiy create without account_id
+curl -X "POST" "http://localhost:3000/identities" \
+     -H 'Content-Type: application/json' \
+     -d $'{
+  "first_name": "paul",
+  "last_name": "h"
+}'
+
+## Identity create with email, telepohne
+curl -X "POST" "http://localhost:3000/identities" \
+     -H 'Content-Type: application/json' \
+     -d $'{
+  "email_address": {
+    "email": "paul@example.com",
+    "preferred": "false"
+  },
+  "last_name": "h",
+  "telephone": {
+    "preferred": "true",
+    "phone_number": "+1(123)555-5555",
+    "allow_sms": "true",
+    "type": "mobile"
+  },
+  "first_name": "paul"
+}'
+
+## Identity Update
+curl -X "PATCH" "http://localhost:3000/identities/3594f042-4ae1-4843-842e-4fb912cbe7b7" \
+     -H 'Content-Type: application/json' \
+     -d $'{
+  "email_address": {
+    "email": "paul-new-email@example.com",
+    "preferred": "false"
+  },
+  "last_name": "new email",
+  "telephone": {
+    "preferred": "true",
+    "phone_number": "1235555555",
+    "allow_sms": "true",
+    "type": "mobile"
+  },
+  "account_id": "1342ae0d-2aeb-4537-a9ab-fbf43235c6d1",
+  "first_name": "bob new email"
+}'
+
+## Identity Destroy
+curl -X "DELETE" "http://localhost:3000/identities/31e94b23-af1d-4c25-acac-d8365767b1bb"
+
+## Vehicles List
+curl "http://localhost:3000/vehicles"
+
+## Vehicle Create Simple
+curl -X "POST" "http://localhost:3000/vehicles" \
+     -H 'Content-Type: application/json; charset=utf-8' \
+     -d $'{
+  "vin": "1234",
+  "year": "1996",
+  "account_id": "665b135f-e273-49ce-abe9-0b142c5622ff"
+}'
+
+## Vehicle Create Full
+curl -X "POST" "http://localhost:3000/vehicles" \
+     -H 'Content-Type: application/json; charset=utf-8' \
+     -d $'{
+  "color": "Black",
+  "model": "M3",
+  "vin": "1234",
+  "year": "1996",
+  "account_id": "1342ae0d-2aeb-4537-a9ab-fbf43235c6d1",
+  "make": "BMW"
+}'
+
+## Vehicle Update
+curl -X "PATCH" "http://localhost:3000/vehicles/829362d8-5ac7-4d4f-b832-1086b3576139" \
+     -H 'Content-Type: application/json; charset=utf-8' \
+     -d $'{
+  "color": "Sky Blue",
+  "model": "M3",
+  "vin": "1234",
+  "year": "1996",
+  "account_id": "1342ae0d-2aeb-4537-a9ab-fbf43235c6d1",
+  "make": "BMW"
+}'
+
+## Vehicle Destroy
+curl -X "DELETE" "http://localhost:3000/vehicles/0cfa0b22-661b-49a8-abc0-bc3d598eb286"
